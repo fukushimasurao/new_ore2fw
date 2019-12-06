182209c823c76918e522f99e0a889464ec2931af
HayatoFukushima <nu0640042@gmail.com>
2019-12-07 00:22:11 +0900

    make it better

--- public/index.php
+++ public/index.php
@@ -9,9 +9,6 @@ if ($_SERVER['REQUEST_URI'] === '/') {
     echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
     echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
     echo "<hr>\n";
-    
-    
-    var_dump($_SERVER['REQUEST_URI']);
     exit;
 }
 //　↓今回追加した部分↓
