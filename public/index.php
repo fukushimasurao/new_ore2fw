aaab6bfe402541c812ec10729960dd0c36b159f4
HayatoFukushima <nu0640042@gmail.com>
2019-12-07 00:17:44 +0900

    make it better

--- public/index.php
+++ public/index.php
@@ -3,23 +3,25 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // echo "this is test \n";
 // echo h(date('Y/m/d'));
-if ($_SERVER['REQUEST_URI'] === '/') {
+if ($_SERVER['REQUEST_URI'] === '/aa') {
     echo "<!DOCTYPE html>\n";
     echo "<title>test</title>\n";
     echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
     echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
     echo "<hr>\n";
+    
+    
+    var_dump($_SERVER['REQUEST_URI']);
     exit;
 }
 //　↓今回追加した部分↓
-if ($_SERVER['REQUEST_URI'] === '/phpinfo.php') {
+if ($_SERVER['REQUEST_URI'] === '/phpinfo') {
+    // var_dump($_SERVER['REQUEST_URI']);
+    // echo "heoppppp";
     phpinfo();
     exit;
 }
 //　↑今回追加した部分↑
-
-http_response_code(404);
-echo "<p>404 Not Found</p>";
 
 http_response_code(404);
 echo "<p>404 Not Foundだぞ</p>";
