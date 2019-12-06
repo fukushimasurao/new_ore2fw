75dc409c2c15c315040548a32bbc9dd03f64e9f8
HayatoFukushima <nu0640042@gmail.com>
2019-12-06 21:49:40 +0900

    make it better

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-echo h(date('Y / m / d'));
+
+// echo "this is test \n";
+// echo h(date('Y/m/d'));
+if ($_SERVER['REQUEST_URI'] === '/') {
+    echo "<!DOCTYPE html>\n";
+    echo "<title>test</title>\n";
+    echo "this is test \n";
+    echo "h(date('Y/m/d'))";
+    exit;
+}
+http_response_code(404);
+echo "<p>404 Not Found</p>";
