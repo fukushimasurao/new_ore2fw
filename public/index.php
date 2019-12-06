928bafdb370142afcd16a432c0ef37c71aad56d8
HayatoFukushima <nu0640042@gmail.com>
2019-12-06 23:55:30 +0900

    make it better

--- public/index.php
+++ public/index.php
@@ -6,14 +6,9 @@ require __DIR__ . '/../vendor/autoload.php';
 if ($_SERVER['REQUEST_URI'] === '/') {
     echo "<!DOCTYPE html>\n";
     echo "<title>test</title>\n";
-    echo "this is test \n";
-    echo "h(date('Y/m/d'))";
-
-    exit;
-}
-
-if ($_SERVER['REQUEST_URI'] === '/phpinfo.php') {
-    phpinfo();
+    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
+    echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+    echo "<hr>\n";
     exit;
 }
 http_response_code(404);
