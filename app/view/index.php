eeba34567a68746b66c49e814a2640918b2f294a
fukushima hayao <nu0640042@gmail.con>
2019-12-08 05:21:37 +0900

    ちょっと修正

--- app/view/index.php
+++ app/view/index.php
@@ -3,3 +3,5 @@ echo "<!DOCTYPE html>\n";
 echo "<title>test</title>\n";
 echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
 echo "<ul><li><a href='/phpinfo'><code>phpinfo()</code></a></ul>\n";
+
+echo "aadfsdfwdfsf";
